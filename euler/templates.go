c0830711441e4ec14d060d4d855d15bbc9592b86
Jacob Hands <g@gstx.co>
2015-12-15 16:02:58 -0700

    Template adjustment

--- euler/templates.go
+++ euler/templates.go
@@ -10,10 +10,7 @@ import "github.com/jacobhands/pu"
 
 var (
 	answer  = "NA" // Change to correct answer once solved.
-	problem = pu.Problem{
-		ID:            1,
-		Solver:        solve,
-		CorrectAnswer: answer}
+	problem = pu.Problem{ID: 1, Solver: solve, CorrectAnswer: answer}
 )
 
 func main() {
