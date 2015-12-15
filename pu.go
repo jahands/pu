4908e914407a4447f93ca8ca28616a274ea14b5a
Jacob Hands <g@gstx.co>
2015-12-15 11:38:31 -0700

    Rename Solve to Solver for clarity

--- pu.go
+++ pu.go
@@ -11,23 +11,23 @@ import (
 // Problem is a Project Euler (.net) problem
 type Problem struct {
 	ID            int
-	Solve         func() string
+	Solver        func() string
 	CorrectAnswer string
 }
 
 // Bench benchmarks the problem. Great for testing for improvements.
 func (p Problem) Bench(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		p.Solve()
+		p.Solver()
 	}
 }
 
 // Answer prints out the answer for viewing.
 func (p Problem) Answer() {
-	fmt.Println("Answer to problem " + strconv.Itoa(p.ID) + ": " + p.Solve())
+	fmt.Println("Answer to problem " + strconv.Itoa(p.ID) + ": " + p.Solver())
 }
 
 // Test ensures that the answer is correct. Will fail until problem is solved.
 func (p Problem) Test(t *testing.T) {
-	assert.Equal(t, p.CorrectAnswer, p.Solve(), "These should be equal. Either it hasn't been solved or something broke.")
+	assert.Equal(t, p.CorrectAnswer, p.Solver(), "These should be equal. Either it hasn't been solved or something broke.")
 }
